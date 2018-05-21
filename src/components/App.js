91902c137c96d455dd6e30d277a9bd7a1f9098db
Dwire <gjdwyer8@gmail.com>
2018-05-20 21:39:36 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,43 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+
+class App extends React.Component {
+  state = {
+    filters: [],
+    fruit: [],
+    currentFilter: null
+  }
+
+
+  handleFilterChange = event => {
+    console.log('new filter: ', event.target.value);
+    this.setState({ currentFilter: event.target.value });
+  }
+
+  componentDidMount() {
+    this.fetchFilters();
+    this.fetchFruit();
+  }
+
+  fetchFilters = () => {
+    fetch('/api/fruit_types')
+      .then(response => response.json())
+      .then(filters => this.setState({ filters }));
+  }
+
+  fetchFruit = () => {
+    fetch('/api/fruit')
+      .then(response => response.json())
+      .then(fruit => this.setState({ fruit }));
+  }
+
+
+  render(){
+    return(
+      <FruitBasket handleChange={this.handleFilterChange} filter={this.state.currentFilter} filt={this.state.filters} item={this.state.fruit}/>
+    )
+  }
+}
 
 export default App;
--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -1,29 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FilteredFruitList extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      items: []
-    };
-  }
+const FilteredFruitList = (props) => {
 
-  componentDidMount() {
-    fetch('/api/fruit')
-      .then(response => response.json())
-      .then(items => this.setState({ items }));
-  }
-
-  render() {
-    const list = !this.props.filter || this.props.filter === 'all' ? this.state.items : this.state.items.filter(i => i.fruit_type === this.props.filter);
+    const list = !props.filter || props.filter === 'all' ? props.item : props.item.filter(i => i.fruit_type === props.filter);
 
     return (
       <ul className="fruit-list">
         {list.map((item, index) => <li key={index}>{item.char}</li>)}
       </ul>
     );
-  }
+
 }
 
 export default FilteredFruitList;
